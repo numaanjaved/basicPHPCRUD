41ddfd81705c73e73555dbba5f36c156c6360c7d
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2025-01-15 11:13:07 +0500

    Navbar Added

--- Assets/views/partials/navbar.php
+++ Assets/views/partials/navbar.php
@@ -4,9 +4,12 @@
                 class="fa-solid fa-address-card"></i>
             <span class="logoName">Profiles</span>
         </div>
-        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0 gap-3">
             <li><a href="/basicPHPCRUD/" class="nav-link px-3 <?= ($_SERVER['REQUEST_URI'] == '/basicPHPCRUD/' || $_SERVER['REQUEST_URI'] == '/basicPHPCRUD/home' ? 'activeLink' : 'navLinks'); ?>">Home</a></li>
-            <li><a href="/basicPHPCRUD/records" class="nav-link px-3 <?php echo ($_SERVER['REQUEST_URI'] == '/basicPHPCRUD/records' ? 'activeLink' : 'navLinks'); ?>">Records</a></li>
+            <li><a href="#" class="nav-link px-3 navLinks">Create</a></li>
+            <li><a href="#" class="nav-link px-3 navLinks">Read</a></li>
+            <li><a href="#" class="nav-link px-3 navLinks">Update</a></li>
+            <li><a href="#" class="nav-link px-3 navLinks">Delete</a></li>
         </ul>
         <button class="btn btn-danger logout_btn">Logout</button>
     </nav>
