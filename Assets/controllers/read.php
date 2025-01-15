517fa16dbfd587dd4da5b273d267a0f71a883197
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2025-01-15 12:10:05 +0500

    Read Page Loading

---
+++ Assets/controllers/read.php
@@ -0,0 +1,4 @@
+<?php
+
+views('read.view.php');
+exit();
--- routes.php
+++ routes.php
@@ -3,4 +3,4 @@ $router->get('/basicPHPCRUD', basePath('Assets/controllers/home.php'));
 $router->post('/basicPHPCRUD', basePath('Assets/controllers/data/validate.php'));
 $router->get('/basicPHPCRUD/home', basePath('Assets/controllers/home.php'));
 $router->post('/basicPHPCRUD/home', basePath('Assets/controllers/data/validate.php'));
-$router->get('/basicPHPCRUD/records', basePath('Assets/controllers/records.php'));
+$router->get('/basicPHPCRUD/read', basePath('Assets/controllers/read.php'));
