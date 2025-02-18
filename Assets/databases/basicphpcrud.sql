c62ed3dc7c5f5c6118bf42462361870bf0f77562
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2025-02-18 09:36:18 +0500

    SQL file updated

--- Assets/databases/basicphpcrud.sql
+++ Assets/databases/basicphpcrud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Feb 13, 2025 at 05:25 AM
+-- Generation Time: Feb 18, 2025 at 05:35 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -47,8 +47,19 @@ CREATE TABLE `records` (
 --
 
 INSERT INTO `records` (`user_id`, `firstname`, `email`, `contact`, `address`, `bio`, `user_type`, `admin_name`, `admin_password`, `image_path`, `image_name`, `lastname`) VALUES
-('prof001', 'Ahmed', 'ahmedtahiri.webdev@gmail.com', '03135554563', 'Hospital Road, Chakwal, Punjab, Pakistan', 'Hello, My self Muhammad Ahmed Tahiri.\r\nI am full stack web developer.', 'admin', 'ahmed', '$2y$10$pj8/hsf4CR1BRQ6FtAtZAuFrIbsr7jmUAwWHbn99IEPInvO59xDKm', 'uploads/IMG_5019 Copy.jpg', 'E:\\xampp\\tmp\\phpE40F.tmp', 'Tahiri'),
-('prof002', 'Usman', 'ali@gmail.com', '2131231211', 'adsdsadas', 'adadas', 'user', '', '', 'uploads/IMG_5075.jpg', 'E:\\xampp\\tmp\\php826E.tmp', 'Ali');
+('prof001', 'Ahmed', 'ahmedtahiri.webdev@gmail.com', '03135554563', 'Islamabad', 'Hey, I am Ahmed Tahiri, Professional full stack web developer.\r\nGood to see you here!', 'admin', 'ahmed', '$2y$10$q49CTFhSCTPtXXRZevY2t./Zou.y8I3r6QzIii.zNcu8bhvvk9ZuW', 'uploads/IMG_5019 Copy.jpg', 'E:\\xampp\\tmp\\php14EC.tmp', 'Tahiri');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temp_otp`
+--
+
+CREATE TABLE `temp_otp` (
+  `otp_id` int(11) NOT NULL,
+  `otp_code` int(11) NOT NULL,
+  `user_id` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -58,8 +69,23 @@ INSERT INTO `records` (`user_id`, `firstname`, `email`, `contact`, `address`, `b
 -- Indexes for table `records`
 --
 ALTER TABLE `records`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `temp_otp`
+--
+ALTER TABLE `temp_otp`
+  ADD PRIMARY KEY (`otp_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `temp_otp`
+--
+ALTER TABLE `temp_otp`
+  MODIFY `otp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
