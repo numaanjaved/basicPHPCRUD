2790fe8a8e41b6f211c352fb37c66b3c6a04a336
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2025-02-20 11:32:10 +0500

    Create with admin completed

--- Assets/controllers/CRUD/create.php
+++ Assets/controllers/CRUD/create.php
@@ -1,3 +1,10 @@
 <?php
-views('CRUD/create.view.php');
-exit();
+require_once('Assets/controllers/getLoggedInUser.php');
+if (getLoggedInUser()['user_type'] === 'admin') {
+    views('CRUD/create.view.php');
+    exit();
+} else {
+    $_SESSION['authError'] = 'You are not authorized to perform this action';
+    header('location: /basicPHPCRUD/home');
+    die();
+}
